9932f04b4261fa7dd8e08415ef46637052c1aa9f
annabellamiller <69117318+annabellamiller@users.noreply.github.com>
2023-07-11 12:23:44 +0900

    navbar

--- nav.php
+++ nav.php
@@ -1,16 +1,18 @@
 <div class='navbar'>
-  <a href='/rottensushi/login/logout.php'>Logout</a>
-  <a href='/rottensushi/crud-Purchase/view-purchase.php'>My Movies</a>
-  <a href='/rottensushi/cart/cart.php'>Cart</a>
-  <a href='/rottensushi/crud-Actor/add-actor.php'>Add Actor</a>
-  <a href='/rottensushi/crud-Actor/view-actor.php'>View Actors</a>
-  <a href='/rottensushi/crud-Movie/add-movie.php'>Add Movie</a>
-  <a href='/rottensushi/crud-Review/add-review.php'>Add Review</a>
+  <div class="navbar-right">
+    <a href='/rottensushi/login/logout.php'>Logout</a>
+    <a href='/rottensushi/crud-Purchase/view-purchase.php'>My Movies</a>
+    <a href='/rottensushi/cart/cart.php'>Cart</a>
+    <a href='/rottensushi/crud-Actor/add-actor.php'>Add Actor</a>
+    <a href='/rottensushi/crud-Actor/view-actor.php'>View Actors</a>
+    <a href='/rottensushi/crud-Movie/add-movie.php'>Add Movie</a>
+    <a href='/rottensushi/crud-Review/add-review.php'>Add Review</a>
+  </div>
   <div class="navbar-left">
     <a href='/rottensushi/home.php'>Rotten Sushi</a>
     <a href='/rottensushi/crud-User/view-user.php'>
       <div class="logo-image">
-        <img src='\RottenSushi\Images\logo.jpg' alt='Rotten Sushi Logo' style="display: flex; align-items: center; width: 30px; height: auto; margin-right: 10px;">
+        <img src='\RottenSushi\Images\logo.jpg' alt='Rotten Sushi Logo' style="display: flex; align-items: center; width: 30px; height: auto; margin-right: 10px; padding:0px;">
       </div>
     </a>
   </div>
